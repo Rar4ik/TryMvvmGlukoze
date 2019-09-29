8a5ee9c7fdfdd40012d3b9b5ade3250d8e1d3e84
Rar4ik <Rar4ik@github.com>
2019-09-29 14:05:17 +0300

    Данные передаются

--- TryMvvmGlukoze/Models/CalcConcentrationModel.cs
+++ TryMvvmGlukoze/Models/CalcConcentrationModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TryMvvmGlukoze.Models;
 
 namespace TryMvvmGlukoze
 {
@@ -11,10 +12,9 @@ namespace TryMvvmGlukoze
         private const int coeff = 100;
         private double biggerV = 0;
         private double lesserV = 0;
-        private double _theFuckingDose;
-        public int GetBiggerAmount(/*string dose, */string lesserCon, string biggerCon, string overallV)
+        public int GetBiggerAmount(string lesserCon, string biggerCon, string overallV)
         {
-            biggerV = (_theFuckingDose * coeff - Convert.ToInt32(lesserCon) * Convert.ToInt32(overallV)) 
+            biggerV = (DoseObject.SendDose() * coeff - Convert.ToInt32(lesserCon) * Convert.ToInt32(overallV)) 
                 / (Convert.ToInt32(biggerCon) - Convert.ToInt32(lesserCon));
             return (int)biggerV;
         }
@@ -22,10 +22,6 @@ namespace TryMvvmGlukoze
         {
             lesserV = Convert.ToInt32(overallV) - Convert.ToInt32(biggerV);
             return (int)lesserV;
-        }
-        public void GetTHEFUCKINGDose(ref double theFdose)
-        {
-            _theFuckingDose = theFdose;
         }
     }
 }
--- TryMvvmGlukoze/Models/CalcDoseModel.cs
+++ TryMvvmGlukoze/Models/CalcDoseModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TryMvvmGlukoze.Models;
 
 namespace TryMvvmGlukoze
 {
@@ -10,12 +11,11 @@ namespace TryMvvmGlukoze
     {
         private const double coeff = 1.44;
         private double _vvDose;
-        CalcConcentrationModel CalcConcentrationModel = new CalcConcentrationModel();
         public double GetDose(string mass, string speed) => Convert.ToDouble(speed) * (Convert.ToDouble(mass) / 1000) * coeff;
         public double GetVVDose(string dose, string enteralno)
         {
             _vvDose = Convert.ToDouble(dose) - Convert.ToDouble(enteralno);
-            CalcConcentrationModel.GetTHEFUCKINGDose(ref _vvDose);
+            DoseObject.GetDose(_vvDose);
             return _vvDose;
         }
     }
