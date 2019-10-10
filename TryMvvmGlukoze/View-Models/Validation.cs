e7ed3ad902dd0236f662975fee9a5e0a2a2669c4
Rar4ik <Rar4ik@github.com>
2019-10-10 18:27:20 +0300

    Версия 1.0 готова

---
+++ TryMvvmGlukoze/View-Models/Validation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryMvvmGlukoze.View_Models
+{
+    class Validation
+    {
+        public bool CheckLenght(string parametr)
+        {
+            if (parametr == null)
+                return false;
+            else return true;
+        }
+        public bool CheckChar(string parametr)
+        {
+            bool IsValid;
+            double number;
+            IsValid = double.TryParse(parametr, out number);
+            if (IsValid == false)
+                return false;
+            else return true;
+        }
+    }
+}
